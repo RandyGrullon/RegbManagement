5fb6cf85de0677e4167faeb3833f21cb7fc59909
randy.grullon <randy6grullon@gmail.com>
2023-03-02 15:49:58 -0400

    creating the FE

--- FE/app/src/app/app.component.ts
+++ FE/app/src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
-
+import { Menu } from './models/menu';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'RegbManagement';
+  menuArray: Menu[] = [
+    {
+      id: 1,
+      name: 'Test Menu',
+      description: 'Test Description',
+      averageRating_Value: '5',
+      averageRating_NumRatings: '1',
+      hostId: 1,
+      createdDateTime: '2019-01-01',
+      updatedDateTime: '2019-01-01',
+    },
+    {
+      id: 2,
+      name: 'Test Menu 2',
+      description: 'Test Description 2',
+      averageRating_Value: '5',
+      averageRating_NumRatings: '1',
+      hostId: 2,
+      createdDateTime: '2019-01-01',
+      updatedDateTime: '2019-01-01',
+    },
+    {
+      id: 3,
+      name: 'Test Menu 3',
+      description: 'Test Description 3',
+      averageRating_Value: '5',
+      averageRating_NumRatings: '1',
+      hostId: 3,
+      createdDateTime: '2019-01-01',
+      updatedDateTime: '2019-01-01',
+    }
+  ];
 }
---
+++ FE/app/src/app/models/menu.ts
@@ -0,0 +1,10 @@
+export class Menu {
+    id: number;
+    name: string;
+    description: string;
+    averageRating_Value: string;
+    averageRating_NumRatings: string;
+    hostId: number;
+    createdDateTime: string;
+    updatedDateTime: string;
+}
