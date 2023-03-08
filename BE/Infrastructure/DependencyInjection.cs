65df32f0ea0a2622384b6226a3a96aa14c784e2b
randy.grullon <randy6grullon@gmail.com>
2023-03-08 15:26:10 -0400

    adding the finishing to the docker compose

--- BE/Infrastructure/DependencyInjection.cs
+++ BE/Infrastructure/DependencyInjection.cs
@@ -21,32 +21,35 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(
         this IServiceCollection services,
-        ConfigurationManager configuration)
+        ConfigurationManager configuration
+    )
     {
-        services
-            .AddAuth(configuration)
-            .AddPersistance();
+        services.AddAuth(configuration).AddPersistance();
 
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
         return services;
     }
 
-    public static IServiceCollection AddPersistance(
-        this IServiceCollection services)
+    public static IServiceCollection AddPersistance(this IServiceCollection services)
     {
-        services.AddDbContext<RegbDbContext>(options =>
-            options.UseSqlServer("Server=localhost;Database=Regb;User Id=SA;Password=;TrustServerCertificate=true"));
+        services.AddDbContext<RegbDbContext>(
+            options =>
+                options.UseNpgsql(
+                    "Host=localhost;Port=5432;Database=regbdb;Username=root;Password=root"
+                )
+        );
 
-        services.AddScoped<IUserRepository, Persistence.UserRepository>();
-        services.AddScoped<IMenuRepository, Persistence.MenuRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IMenuRepository, MenuRepository>();
 
         return services;
     }
 
     public static IServiceCollection AddAuth(
-            this IServiceCollection services,
-            ConfigurationManager configuration)
+        this IServiceCollection services,
+        ConfigurationManager configuration
+    )
     {
         var jwtSettings = new JwtSettings();
         configuration.Bind(JwtSettings.SectionName, jwtSettings);
@@ -54,18 +57,23 @@ public static class DependencyInjection
         services.AddSingleton(Options.Create(jwtSettings));
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
 
-        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = jwtSettings.Issuer,
-                ValidAudience = jwtSettings.Audience,
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(jwtSettings.Secret)),
-            });
+        services
+            .AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(
+                options =>
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidAudience = jwtSettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(jwtSettings.Secret)
+                        ),
+                    }
+            );
 
         return services;
     }
